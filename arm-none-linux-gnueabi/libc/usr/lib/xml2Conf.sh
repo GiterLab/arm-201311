2db1f1e5e0334da6de1cd92aa32fc62865baeeeb
tobyzxj <toby.zxj@gmail.com>
2015-02-05 14:19:17 +0800

    add libxml2-2.9.2

---
+++ arm-none-linux-gnueabi/libc/usr/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/tobyzxj/tools/arm-2013.11/arm-none-linux-gnueabi/libc/usr/lib"
+XML2_LIBS="-lxml2 -lz   -lm "
+XML2_INCLUDEDIR="-I/home/tobyzxj/tools/arm-2013.11/arm-none-linux-gnueabi/libc/usr/include/libxml2"
+MODULE_VERSION="xml2-2.9.2"
+
